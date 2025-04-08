2d5db718649853579e288797b82a20f4336943d6
Sanjay-S31 <127613655+Sanjay-S31@users.noreply.github.com>
2025-04-08 13:09:57 +0530

    Forgot Password feature added

---
+++ backend/utils/mailer.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+function sendMail(to, subject, text) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.error('Error sending email:', error);
+        }
+        console.log('Email sent successfully:', info.response);
+    });
+}
+
+module.exports = sendMail;
